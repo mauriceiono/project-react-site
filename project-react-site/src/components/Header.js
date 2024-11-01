c2c0f274d472b94bbe5966c9508a5eda6de465d4
mauriciono <mthooks@email.sc.edu>
2024-11-01 17:45:08 -0400

    ready to deploy

--- project-react-site/src/components/Header.js
+++ project-react-site/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
@@ -6,10 +7,10 @@ const Header = () => {
             <h1>Video Game Character Hub</h1>
             <nav>
                 <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/characters">Characters</a></li>
-                    <li><a href="/games">Games</a></li>
-                    <li><a href="/about">About</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/characters">Characters</Link></li>
+                    <li><Link to="/games">Games</Link></li>
+                    <li><Link to="/about">About</Link></li>
                 </ul>
             </nav>
         </header>
