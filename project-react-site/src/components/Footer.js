2bf5eb539ee49278c52a41a485c0482f7ef02079
mauriciono <mthooks@email.sc.edu>
2024-11-02 13:25:23 -0400

    ready to deploy

--- project-react-site/src/components/Footer.js
+++ project-react-site/src/components/Footer.js
@@ -3,6 +3,14 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
+            <div className="footer-content">
+                <p>Follow us on:</p>
+                <ul className="social-media">
+                    <li><a href="#"><img src="images/ig logo.jpg" alt="Instagram" /></a></li>
+                    <li><a href="#"><img src="images/x.jpg" alt="Twitter" /></a></li>
+                    <li><a href="#"><img src="images/face.jpg" alt="Facebook" /></a></li>
+                </ul>
+            </div>
             <p>&copy; 2024 Video Game Character Hub</p>
         </footer>
     );
