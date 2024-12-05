f80676cdb9e42d0e7fd91c840b89b423595f1291
mauriciono <mthooks@email.sc.edu>
2024-12-05 16:47:40 -0500

    testing changes

--- project-react-site/src/pages/Characters.js
+++ project-react-site/src/pages/Characters.js
@@ -12,7 +12,7 @@ const Characters = () => (
                     Explore iconic video game characters across different universes. Click on any character to learn more about their abilities, background, and role in their game. or add your own
                 </p>
                 <Link to="/add-character" className="add-character-button"> Add New Character</Link>
-                <Link to="/characters-list" className="add-character-button"> New CharacterList</Link>
+                <Link to="/characters-list" className="add-character-button"> CharacterList</Link>
                 <Link to="/edit-character" className="add-character-button">Edit Character</Link>
                 <Link to="/delete-character" className="add-character-button">Delete Character</Link>
             </div>
